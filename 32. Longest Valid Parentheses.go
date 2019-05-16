6b0c43712c9dce1fa6f118016a5b1b1740232546
kk <kurtqin@tencent.com>
2019-05-16 20:32:39 +0800

    update 32

--- 32. Longest Valid Parentheses.go
+++ 32. Longest Valid Parentheses.go
@@ -2,19 +2,25 @@ package main
 
 func longestValidParentheses(s string) int {
 	var result = 0
-	var stack []string
-	var stackindex []int
-	var index = 0
-	for {
-		if index >= len(s) {
-			break
-		}
-		if s[index] == "(" {
-			stack.
-		}
+
+	var dp []int = make([]int, len(s)+1)
+	if len(dp) == 0 {
+		return 0
 	}
+	dp[0] = 0
+	for i := 1; i <= len(s); i++ {
+		j := i - dp[i-1] - 2
+		if j < 0 || s[i-1] == '(' || s[j] == ')' {
+			dp[i] = 0
+			continue
+		} else {
+			dp[i] = dp[i-1] + 2 + dp[j]
+			if result < dp[i] {
+				result = dp[i]
+			}
+		}
 
-
+	}
 
 	return result
 }
